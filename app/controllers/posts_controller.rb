0848df05aef5d26a14d64d98bf4b9432e1fa83b1
Yu Okuda <yu.kyoto2720@gmail.com>
2018-10-31 03:57:23 +0900

    postとuserの紐付けを完成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post
+    end
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    @user = User.find_by(id: @post.user_id)
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.title = params[:title]
+    @post.content = params[:content]
+    if @post.save
+      redirect_to @post
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to root_url
+  end
+
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if current_user.id != @post.user_id
+      alert = "Not yours"
+      redirect_to root_url
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :microposts
-  
+  has_many :posts
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
